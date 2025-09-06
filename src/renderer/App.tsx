1a0919230e27738890e3536135c0f4df40bb85a7
Civiana <clydebot@outlook.sa>
2025-09-06 19:45:09 +0300

    some aids stuff

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,48 +1,45 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState } from 'react';
 import './App.css';
+import { evaluate } from 'mathjs';
 
 function Hello() {
+  const [num, setNum] = useState(''); // store input as string
+
+  const addoutput = (input) => {
+    setNum((prev) => prev + input); // append input as string
+  };
+
+  const calculate = () => {
+    const result = evaluate(num);
+    setNum(result.toString());
+  }
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
+      <div className='Answer'>
+        <input type='text' value={num} readOnly />
       </div>
-      <h1>Alsalam zaki boiler plate is ready</h1>
-      <p>
-        please go back to the steps i gave you the other day and start from
-        there , please use anything and everything that makes your job easier ,
-        use shadcnui for components , dont create from scratch, its hard and
-        annoying.
-      </p>
-
-      <p>Also install tailwindcss for easy styling.</p>
-
-      <p>
-        Dont forget to ask me anything you get stuck on for more than 15 mins
-      </p>
-      <p>starting point of your app is at src/renderer/App.tsx</p>
-
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
+      <div className='buttonsier'>
+        <button onClick={() => addoutput('1')}>1</button>
+        <button onClick={() => addoutput('2')}>2</button>
+        <button onClick={() => addoutput('3')}>3</button>
+        <button onClick={() => addoutput('4')}>4</button>
+        <button onClick={() => addoutput('5')}>5</button>
+        <button onClick={() => addoutput('6')}>6</button>
+        <button onClick={() => addoutput('7')}>7</button>
+        <button onClick={() => addoutput('8')}>8</button>
+        <button onClick={() => addoutput('9')}>9</button>
+        <button onClick={() => addoutput('-')}>-</button>
+        <button onClick={() => addoutput('0')}>0</button>
+        <button onClick={() => addoutput('+')}>+</button>
+        <button className='equal' onClick={() => calculate()}>=</button>
+        <button onClick={() => setNum('')}>C</button>
       </div>
-      <h1>good luck, inshallah we will finish this and make it good</h1>
     </div>
   );
 }
 
-// this is from baraa, below is a router same as web you privde a path and an element and when that path is accessed, the element will be rendered
 export default function App() {
   return (
     <Router>
