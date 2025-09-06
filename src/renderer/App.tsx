736b7019035930764b2981629b10084d3f7d2550
Baraa <baraa.sameeh.yousef@gmail.com>
2025-09-07 02:34:48 +0800

    make background black for ur eyes wink wink

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import 'tailwindcss/index.css';
 import './App.css';
+import Childcomp from './components/ChildComp';
 
 function Hello() {
   return (
     <div className="flex flex-col h-screen w-screen bg-black min-w-0 min-h-0 overflow-hidden">
+      <Childcomp />
       <p className="bg-blue-950">Hi nice people</p>
     </div>
   );
