dc78b7258bb33bc64f56caf9d7d44eefab374a5f
Baraa <baraa.sameeh.yousef@gmail.com>
2025-10-20 00:19:33 +0800

    ...

--- src/screens/Rooms.tsx
+++ src/screens/Rooms.tsx
@@ -179,6 +179,7 @@ function Rooms() {
       alert('Please enter a signaling server URL');
       return;
     }
+
     destroyYEnvironment(envRef.current!);
     handleJoinRoom(roomName, signalingUrl);
 
