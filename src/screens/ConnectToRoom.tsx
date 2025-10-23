6c8d737673a673b221e608553c729fc8e4392198
baraa <baraa.sameeh.yousef@gmail.com>
2025-10-23 08:53:07 +0800

    ....

--- src/screens/ConnectToRoom.tsx
+++ src/screens/ConnectToRoom.tsx
@@ -151,8 +151,8 @@ function ConnectToRoom() {
           const reject = getYDoc();
           const rejArr = reject?.getArray<string>('rejectedArr');
           rejArr?.delete(i, 1);
-          removeRemoteDevices(deviceID);
-          // disconnect();
+          // removeRemoteDevices(deviceID); it did not work zaki it gives a 404 error and does not delete from syncthing
+          disconnect();
         }
       });
     }
